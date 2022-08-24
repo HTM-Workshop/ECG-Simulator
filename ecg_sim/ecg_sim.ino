b8cb39fdb017bb2f5a6136ebb4b4c15402b0849e
Kevin Williams <williams.kevin@spcollege.edu>
2022-08-24 10:39:33 -0400

    Added license and disclaimer

--- ecg_sim/ecg_sim.ino
+++ ecg_sim/ecg_sim.ino
@@ -1,8 +1,25 @@
 /*
- *      DIY-ECG-SIM
- *   v1.0.0 JUN/28/2022
+ *        DIY-ECG-SIM
+ *     v1.0.0 JUN/28/2022
  *   Written by Kevin Williams
- */
+ *   
+ *   WARNING: This program is for educational purposes only.
+ *                  NOT FOR MEDICAL USE
+ *   
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 //#define ENABLE_CUSTOM_WAVE
 #define ENABLE_RESP_SIM         // comment out to disable resp sim
